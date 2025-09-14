79c2ac7a51f2c8970008de9e47d281f639056543
gabriellemacinnes <gabrielle.macinnes@gmail.com>
2025-09-14 10:58:00 -0400

    m

--- backend/test_pipeline.py
+++ backend/test_pipeline.py
@@ -2,9 +2,9 @@ from pipeline_executor import execute_pipeline
 
 pipeline = {
     "blocks": [
-        {"block_type": "input_source", "block_id": 1, "csv_source": "imdb_top_1000.csv"},
+        {"block_type": "input_source", "block_id": 1, "csv_source": "HistoricalQuotes.csv"},
         {"block_type": "process", "block_id": 2, "pre_req": [1], "prompt": "clean missing values"},
-        {"block_type": "process", "block_id": 3, "pre_req": [2], "prompt": "aggregate movies by genre"},
+        {"block_type": "process", "block_id": 3, "pre_req": [2], "prompt": "find 50 day moving averages of closing price"},
         {"block_type": "destination", "block_id": 4, "pre_req": [3], "email_dest": "user@example.com"}
     ]
 }
